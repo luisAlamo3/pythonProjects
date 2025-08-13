c85a5c66e607ccdb56b75d3dc8307aa9bba52a78
Luis Alamo <luisalamo3@gmail.com>
2025-08-13 15:13:49 -0600

    Se agregan comentarios que sirven para la otra parte de la tarea

--- Tarea2_basico.py
+++ Tarea2_basico.py
@@ -1,3 +1,49 @@
+# 1. Planeación
+# Inicio
+# Solicitar la cantidad de ciudadanos que se atenderán
+# Para cada solicitud:
+# Registrar nombre
+# Registrar tiempo estimado
+
+# si la cantidad de solicitudes aun no es la que se indico al principio sigue solicitando
+# si la cantidad de solicitudes es la que se indico finaliza la solicitud de registro
+
+# manda a ordenar las solicitudes por tiempo estimado de menor a mayor
+
+# mostrar la lista ordenada.
+
+# Fin
+
+# 2. Análisis
+# Entrada de datos:
+# La cantidad máxima de ciudadanos que serán atendidos será de 100 
+# Cada solicitud deberá tener un nombre (máximo 50 caracteres) y un 
+# Tiempo estimado de atención (máximo 120 minutos)
+
+# Proceso:
+# Se deberán ordenar las solicitudes de menor a mayor
+
+# Salida esperada:
+# Una lista de solicitudes (nombre y tiempo de espera estimado) ordenada de menor a mayor por tiempo de espera estimado
+
+# 3. Diseño de Solución
+# Inicio
+# Solicitar la cantidad de ciudadanos que se atenderán
+# generar una lista de dicha cantidad
+# Para cada solicitud:
+# Registrar nombre
+# Registrar tiempo estimado
+# Guardar cada solicitud en un bloque de la lista
+# si la cantidad de solicitudes aun no es la que se indico al principio sigue solicitando
+# si la cantidad de solicitudes es la que se indicó deja de pedir solicitudes
+
+# manda a ordenar las solicitudes por tiempo estimado de menor a mayor
+
+# mostrar la lista ordenada.
+
+# Fin
+
+
 # Versión básica: ingreso manual y sin medición de tiempo
 
 solicitudes = []
