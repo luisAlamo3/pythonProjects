f01c3d4021404f0ca63bf7551de6b882ec49bd2d
Luis Alamo <luisalamo3@gmail.com>
2025-08-13 14:55:24 -0600

    Se agrega la posibilidad de usar datos de prueba o ingresados manualmente y de seleccionar los ciudadanos que se requieran

--- Tarea2.py
+++ Tarea2.py
@@ -1,22 +1,52 @@
-# Solicitamos la cantidad de ciudadanos que realizarán una solicitud
-cantidad_ciudadanos = int(input("Ingrese la cantidad de ciudadanos que realizarán una solicitud: "))
+import random
 
-# Creamos una lista para almacenar las solicitudes de cada ciudadano
+# Datos preestablecidos para pruebas rápidas
+usar_datos_prueba = True  # Cambia a True para usar datos de prueba
+
+nombres_base = [
+    "Ana", "Luis", "Carlos", "María", "José", "Elena", "Pedro", "Lucía", "Miguel", "Sofía",
+    "Juan", "Laura", "Andrés", "Paula", "Diego", "Valeria", "Jorge", "Camila", "Fernando", "Daniela",
+    "Ricardo", "Gabriela", "Raúl", "Patricia", "Roberto", "Sandra", "Alberto", "Mónica", "Hugo", "Carmen",
+    "Manuel", "Rosa", "Francisco", "Teresa", "Guillermo", "Alicia", "Eduardo", "Beatriz", "Felipe", "Verónica",
+    "Oscar", "Silvia", "Martín", "Isabel", "Pablo", "Natalia", "Emilio", "Lorena", "Tomás", "Julia"
+]
+
+datos_prueba = [
+    {
+        "nombre": f"{random.choice(nombres_base)}_{i+1}",
+        "tiempo_estimado": random.randint(10, 120)
+    }
+    for i in range(5000)
+]
+
 solicitudes = []
 
-# Solicitamos la información de cada ciudadano
-for i in range(cantidad_ciudadanos):
-    print(f"\nCiudadano {i + 1}:")
-    nombre = input("Ingrese su nombre: ")
-    tiempo_estimado = int(input("Ingrese el tiempo estimado en minutos (max 120 min): "))
-    
-    ciudadano = {
-        "id": i + 1,
-        "nombre": nombre,
-        "tiempo_estimado": tiempo_estimado
-    }
-    # Agregamos el diccionario a la lista de solicitudes
-    solicitudes.append(ciudadano)
+if usar_datos_prueba:
+    for i, dato in enumerate(datos_prueba):
+        ciudadano = {
+            "id": i + 1,
+            "nombre": dato["nombre"],
+            "tiempo_estimado": dato["tiempo_estimado"]
+        }
+        solicitudes.append(ciudadano)
+    cantidad_ciudadanos = len(datos_prueba)
+else:
+    # Solicitamos la cantidad de ciudadanos que realizarán una solicitud
+    cantidad_ciudadanos = int(input("Ingrese la cantidad de ciudadanos que realizarán una solicitud: "))
+
+    # Solicitamos la información de cada ciudadano
+    for i in range(cantidad_ciudadanos):
+        print(f"\nCiudadano {i + 1}:")
+        nombre = input("Ingrese su nombre: ")
+        tiempo_estimado = int(input("Ingrese el tiempo estimado en minutos (max 120 min): "))
+        
+        ciudadano = {
+            "id": i + 1,
+            "nombre": nombre,
+            "tiempo_estimado": tiempo_estimado
+        }
+        # Agregamos el diccionario a la lista de solicitudes
+        solicitudes.append(ciudadano)
 
 # Ordenamos las solicitudes por tiempo estimado
 #solicitudes.sort(key=lambda x: x['tiempo_estimado'])
